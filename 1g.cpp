08425477287046e7d9fe31dc62fb71b7ed5416e8
balajicr007 <balajicr007@gmail.com>
2017-08-07 09:53:29 +0530

    Create 1g.cpp

---
+++ 1g.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+signed long inp;
+clrscr();
+cin>>inp;
+if(inp>0)
+cout<<"positive";
+else if(inp<0)
+cout<<"negative";
+else
+cout<<"zero";
+getch();
+}
